8ab936db4d59782da54d7e8a358564e3aebd7624
MichaelLELU <lelu.michael@gmail.com>
2024-03-19 12:11:41 +0100

    atelier

---
+++ index.js
@@ -0,0 +1,8 @@
+const firstName = 'Pierre'
+const secondName = 'Pierre'
+const thirdName = 'michael'
+const fourthName = 'yann'
+
+import chalk from 'chalk';
+
+console.log(chalk.blue(firstName),chalk.red(secondName),chalk.green(thirdName),chalk.yellow(fourthName));
